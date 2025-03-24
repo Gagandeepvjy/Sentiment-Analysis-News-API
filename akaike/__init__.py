58f2ae80e84a9edf8413e8799a5f930b90cec61e
Gagandeep <gagandeepvjy41003@gmail.com>
2025-03-24 18:50:07 +0530

    feat: added fastapi configs

--- akaike/__init__.py
+++ akaike/__init__.py
@@ -10,7 +10,7 @@ from fastapi import FastAPI
 from .controllers import news_controller, sentiment_controller, tts_controller
 SECRET_KEY = os.urandom(32)
 
-app = FastAPI()
+app = FastAPI(title = "Akaike Technology - News Sentiment Analysis API")
 app.include_router(news_controller.router, prefix="/api", tags=["News"])
 app.include_router(sentiment_controller.router, prefix="/api", tags=["Sentiment"])
 app.include_router(tts_controller.router, prefix="/api", tags=["Speech"])
