f595542baf3efc31db57b1a7870d49dfe3a15ba3
Gagandeep <gagandeepvjy41003@gmail.com>
2025-03-24 22:31:29 +0530

    chore: added docker file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("akaike:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("akaike:app", host="0.0.0.0", port=7860, reload=True)
